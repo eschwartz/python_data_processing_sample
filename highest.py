020a98897dcd1888e2fa9b70fe6ad0f0d49936cf
Edan Schwartz <edanschwartz@gmail.com>
2022-12-11 14:19:00 -0600

    Remove `if __main__` from highest.py
    It will never be anything else

--- highest.py
+++ highest.py
@@ -6,26 +6,25 @@ from highest_scores.get_high_scores import get_high_scores
 from highest_scores.parse_cli_args import parse_cli_args
 
 
-if __name__ == '__main__':
-    # Get the data file path and max results from the CLI args
-    file_path, max_results = parse_cli_args(sys.argv)
+# Get the data file path and max results from the CLI args
+file_path, max_results = parse_cli_args(sys.argv)
 
-    # Open the data file
-    with open(file_path) as f:
-        try:
-            # Get the top N scores
-            results = get_high_scores(f, max_results)
+# Open the data file
+with open(file_path) as f:
+    try:
+        # Get the top N scores
+        results = get_high_scores(f, max_results)
 
-            # Output results as JSON
-            print(json.dumps(results, indent=2))
-        except DataProcessingException as err:
-            # Handle data processing errors and exit
-            print(err.message, file=sys.stderr)
+        # Output results as JSON
+        print(json.dumps(results, indent=2))
+    except DataProcessingException as err:
+        # Handle data processing errors and exit
+        print(err.message, file=sys.stderr)
 
-            # Exceptions define their own exit code
-            # according to the type of failure
-            sys.exit(err.exit_code)
-        except Exception as err:
-            # Handle other errors (eg. script failure)
-            print(f"Process failed: {err}", file=sys.stderr)
-            sys.exit(1)
+        # Exceptions define their own exit code
+        # according to the type of failure
+        sys.exit(err.exit_code)
+    except Exception as err:
+        # Handle other errors (eg. script failure)
+        print(f"Process failed: {err}", file=sys.stderr)
+        sys.exit(1)
